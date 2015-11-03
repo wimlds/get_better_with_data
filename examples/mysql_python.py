7ac386dfc98afaabca9fa1906bdd6fdc7056bddf
squigley <squigley@shutterstock.com>
2015-11-02 20:43:53 -0500

    update Mysql python example

--- examples/mysql_python.py
+++ examples/mysql_python.py
@@ -10,18 +10,18 @@ def main():
 
     # Setup MySQL connection
     conn = MySQLdb.connect(host='health-db-internet.c6clocfz5zxy.us-east-1.rds.amazonaws.com',
-                          port=3306,
-                          user=db_user,
-                          passwd=pw)
+                           port=3306,
+                           user=db_user,
+                           passwd=pw)
     cur = conn.cursor()
 
     # Execute query
     query = 'SELECT * FROM CMS_open_payments_2013.general_payment_data WHERE Recipient_State="WY"'
-    print 'Running query:  {0};...\n'.format(query)
+    print 'Running query:  {0};\n'.format(query)
     cur.execute(query)
 
     wyoming_data = cur.fetchall()
-    print 'Columns:  {0}\n'.format(wyoming_data[0])
+    print 'Columns:  {0}\n'.format([c[0] for c in cur.description])
     print 'Number of records: {0}'.format(len(wyoming_data))
 
     # Close MySQL connections
