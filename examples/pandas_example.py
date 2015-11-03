ef07ac00540ccb6d992f5eb14f7c507a31e0110d
Shani Offen <soffen@about.com>
2015-11-02 20:23:18 -0500

    adding comments

--- examples/pandas_example.py
+++ examples/pandas_example.py
@@ -1,18 +1,17 @@
+# This code gives an example of how to use pandas to read
+# one of the datasets into a dataframe
+
 import pymysql
 import pandas.io.sql as sql
 import pandas as pd
-import numpy as np
-import re
-from matplotlib import pyplot as plt
-import seaborn as sns
 
+# first connect to the database
 cnxn = pymysql.connect(host='health-db-internet.c6clocfz5zxy.us-east-1.rds.amazonaws.com',
                        port=3306,
                        user='data_hacker',
                        passwd='hack_pw')
 
-wmt_data = pd.DataFrame()
-
+# For example, let's figure out how much each state paid in 2014 for Allopathic care
 sql_command = \
 'select distinct(d.Recipient_State), '\
     'avg(d.Total_Amount_of_Payment_USDollars) '\
@@ -20,9 +19,10 @@ sql_command = \
     'where d.Physician_Specialty like \'{}\' '\
         'group by d.Physician_Specialty, d.Recipient_State;'.format('%Allopathic%')
 
+# This one line does it all!
 print 'starting call to sql'
 df = sql.read_sql(sql_command, cnxn)
 print '\nfinished processing ask\n'
 
-
+# now do your favorite pandas stuff..
 
